6fc3b9e86147663697905aad4440398290943ddd
Ryan Coulter <ryan.coulter0@gmail.com>
2013-02-01 13:29:55 -0500

    Initial Commit
    Created RePo.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using System.Configuration;
+
+namespace FTPCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(7, 54);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(87, 54);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Save Directory";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(6, 22);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(330, 20);
+            this.txtDir.TabIndex = 3;
+            this.txtDir.Text = System.Configuration.ConfigurationManager.AppSettings["filePath"];
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(261, 54);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last update: ";
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.AutoSize = true;
+            this.lblUpdate.Location = new System.Drawing.Point(90, 91);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(33, 13);
+            this.lblUpdate.TabIndex = 6;
+            this.lblUpdate.Text = "None";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "FTPCheck: Stopped";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 115);
+            this.Controls.Add(this.lblUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FTPCheck";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
+using System.Configuration;
+
+
+namespace FTPCheck
+{
+    public partial class Form1 : Form
+    {
+        string lastTime = "";
+        string host = System.Configuration.ConfigurationManager.AppSettings["host"];//ConfigurationSettings.AppSettings["host"];
+        string user = System.Configuration.ConfigurationManager.AppSettings["user"];
+        string pass = System.Configuration.ConfigurationManager.AppSettings["password"];
+        string thePath = System.Configuration.ConfigurationManager.AppSettings["filePath"];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = folderBrowserDialog1.SelectedPath;
+               // System.Configuration.Configuration config =
+             // ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+               // config.AppSettings.Settings.Remove("filePath");
+               // config.AppSettings.Settings.Add("filePath", folderBrowserDialog1.SelectedPath);
+               // config.Save();
+
+
+
+            }
+        }
+
+        private void checkFile()
+        {
+            string zipFilePath = txtDir.Text + "\\Temp\\";
+            FileInfo theZipFile = new FileInfo(zipFilePath + "GWLXML.zip");
+            if (!theZipFile.Exists) { downloadFile(); return; }
+
+            DirectoryInfo extractDir = new DirectoryInfo(zipFilePath + "extract");
+            if (extractDir.Exists)
+            {
+                extractDir.Delete(true);
+            }
+
+            var options = new ReadOptions { StatusMessageWriter = System.Console.Out };
+            using (ZipFile zip = ZipFile.Read(zipFilePath + "GWLXML.zip", options))
+            {
+                zip.ExtractAll(zipFilePath + "extract");
+            }
+
+            DirectoryInfo di = new DirectoryInfo(zipFilePath + "extract");
+            if (di.Exists)
+            {
+                FileInfo xmlFile = new FileInfo(zipFilePath + "extract\\ENTITY.XML");
+                if (xmlFile.Exists)
+                {
+                    lastTime = xmlFile.LastWriteTime.Year.ToString() + "_" + xmlFile.LastWriteTime.Month.ToString() + "_" + xmlFile.LastWriteTime.Day.ToString() + "_" + xmlFile.LastWriteTime.Hour.ToString() + "." + xmlFile.LastWriteTime.Minute.ToString();
+                    lblUpdate.Text = xmlFile.LastWriteTime.ToString();
+                }
+                di.Delete(true);
+            }
+            else
+            {
+                downloadFile();
+            }
+        }
+
+
+        private void downloadFile()
+        {
+            
+            string inputFilePath = txtDir.Text + "\\Temp\\";
+
+            string ftpFullPath = host;
+            WebClient request = new WebClient();
+            request.Credentials = new NetworkCredential(user, pass);
+            byte[] fileData;
+
+            try
+            {
+                fileData = request.DownloadData(ftpFullPath);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(DateTime.Now + " : " + e.Message,"Error connecting to FTP server");
+                return;
+            }
+
+            Directory.CreateDirectory(inputFilePath);
+            FileStream file = File.Create(inputFilePath + "GWLXML.zip");
+            file.Write(fileData, 0, fileData.Length);
+            file.Close();
+
+            DirectoryInfo extractDir = new DirectoryInfo(inputFilePath + "extract");
+            if (extractDir.Exists)
+            {
+                extractDir.Delete(true);
+            }
+            var options = new ReadOptions { StatusMessageWriter = System.Console.Out };
+            using (ZipFile zip = ZipFile.Read(inputFilePath + "GWLXML.zip", options))
+            {
+                // This call to ExtractAll() assumes:
+                //   - none of the entries are password-protected.
+                //   - want to extract all entries to current working directory
+                //   - none of the files in the zip already exist in the directory;
+                //     if they do, the method will throw.
+                zip.ExtractAll(inputFilePath + "extract");
+            }
+            DirectoryInfo di = new DirectoryInfo(inputFilePath + "extract");
+            if (di.Exists)
+            {
+                FileInfo xmlFile = new FileInfo(inputFilePath + "extract\\ENTITY.XML");
+                if (xmlFile.Exists)
+                {
+                    if (xmlFile.LastWriteTime.ToString() != lastTime || lastTime == "")
+                    {
+                        lastTime = xmlFile.LastWriteTime.Year.ToString() + "_" + xmlFile.LastWriteTime.Month.ToString() + "_" + xmlFile.LastWriteTime.Day.ToString() + "_" + xmlFile.LastWriteTime.Hour.ToString() + "." + xmlFile.LastWriteTime.Minute.ToString();
+                        lblUpdate.Text = xmlFile.LastWriteTime.ToString();
+                        string newDir = txtDir.Text +"\\"+ (lastTime.Replace("/", "").Replace(':', '.'));
+                        Directory.CreateDirectory(newDir);
+                        File.Copy(inputFilePath + "GWLXML.zip", newDir + "\\GWLXML.zip",true);
+                    }
+                }
+                di.Delete(true);
+            }
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            Form1.ActiveForm.Text = "FTPCheck: Running";
+            notifyIcon1.Text = "FTPCheck: Running";
+            txtDir.Enabled = false;
+            checkFile();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            Form1.ActiveForm.Text = "FTPCheck: Stopped";
+            notifyIcon1.Text = "FTPCheck: Stopped";
+            txtDir.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            downloadFile();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "FTPCheck";
+            notifyIcon1.BalloonTipText = "FTPCheck is still running. Double-click the tray icon to restore.";
+
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+    }
+}
